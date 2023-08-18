7b3a268084d1cb760157f53e3ca6893aa8736cf2
sachins602 <sachinsapkota4@gmail.com>
2023-08-18 13:40:04 +0545

    add packages card

--- src/pages/user/OurPackages/index.tsx
+++ src/pages/user/OurPackages/index.tsx
@@ -1,7 +1,17 @@
+import PackageCard from '@/components/PackagesCard';
+import { useGetTripsQuery } from '@/hooks/adminTrip.hook';
+
 export function OurPackages() {
+  const { data } = useGetTripsQuery();
   return (
     <div>
-      <h1>About Us</h1>
+      <img src='/activitiesbg.png' />
+
+      <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+        {data?.data
+          ?.filter(data => (data.category === 'package' ? data : null))
+          .map(item => <PackageCard props={item} />)}
+      </div>
     </div>
   );
 }
